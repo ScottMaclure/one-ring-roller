a9835f5307d8699834283d5bdc7d01c32dd55205
Scott Maclure <scott@maclure.com.au>
2014-12-30 15:25:54 +1100

    Support successDice with angular.

--- public/orr/orr-angular.js
+++ public/orr/orr-angular.js
@@ -1,25 +1,38 @@
 var orrApp = angular.module('orrApp', []);
 
 orrApp.service('diceService', function () {
-	return {
 
-		rollDie: function (dieType) {
-			return Math.floor(Math.random() * (dieType)) + 1;
-		},
+	/**
+	 * Private helper function.
+	 */
+	function rollDie(dieType) {
+		return Math.floor(Math.random() * (dieType)) + 1;
+	}
+
+	// Public API
+	return {
 
 		rollFeatDice: function (featDice) {
 			var d12s = [];
 			switch (featDice) {
 			case 1:
-				d12s.push({ value: this.rollDie(12) });
+				d12s.push({ value: rollDie(12) });
 				break;
 			case 2:
 			case 3:
-				d12s.push({ value: this.rollDie(12) });
-				d12s.push({ value: this.rollDie(12) });
+				d12s.push({ value: rollDie(12) });
+				d12s.push({ value: rollDie(12) });
 				break;
 			}
 			return d12s;
+		},
+
+		rollSuccessDice: function (successDice) {
+			var d6s = [];
+			for (var i = 0; i < successDice; i++) {
+				d6s.push({ value: rollDie(6) });
+			}
+			return d6s;
 		}
 
 	};
@@ -29,6 +42,7 @@ orrApp.controller('OrrCtrl', function ($scope, diceService) {
 
 	// Define initial data setup.
 	$scope.featDice = 1;
+	$scope.successDice = 3;
 	$scope.isWeary = false;
 	$scope.isEnemy = false;
 	$scope.result = {};
@@ -41,6 +55,8 @@ orrApp.controller('OrrCtrl', function ($scope, diceService) {
 		var d12s = diceService.rollFeatDice(featDice);
 
 		// Roll Success dice.
+		var successDice = parseInt($scope.successDice, 10);
+		var d6s = diceService.rollSuccessDice(successDice);
 
 		// Calculate final result.
 
@@ -49,11 +65,7 @@ orrApp.controller('OrrCtrl', function ($scope, diceService) {
 			// This tells the UI that the dice have been rolled.
 			dateStamp: new Date(),
 			d12s: d12s,
-			d6s: [
-				{ value: diceService.rollDie(6) },
-				{ value: diceService.rollDie(6) },
-				{ value: diceService.rollDie(6) }
-			]
+			d6s: d6s
 		};
 
 	}
