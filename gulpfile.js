f6ae6aff812b3750abfcce0937a7775065bc4565
Scott Maclure <scott@maclure.com.au>
2015-01-02 14:50:44 +1100

    Updating gulp and added source folder. Everything in public is temporary now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var sprite = require('css-sprite').stream;
+var del = require('del');
+var runSequence = require('run-sequence');
 
-// generate sprite.png and _sprite.scss
-gulp.task('sprites-dice', function () {
-  return gulp.src('images/dice/*.png')
-	.pipe(sprite({
-		name: 'sprite-dice',
-		style: 'sprite-dice.css',
-		cssPath: '',
-		processor: 'css',
-		prefix: 'dice'
-	}))
-	.pipe(gulpif('*.png', gulp.dest('public/orr'), gulp.dest('public/orr')))
+// Scrub public folder every time, get rid of orphaned files.
+gulp.task('clean', function (cb) {
+  del([
+    'public/**',
+  ], cb);
+});
+
+gulp.task('copy-source-assets', function () {
+	return gulp.src([
+		'source/favicon.ico',
+		'source/index.html',
+		// All files in here get copied, for now.
+		'source/orr/',
+	])
+	.pipe(gulp.dest('public/'))
+});
+
+gulp.task('copy-source-code', function () {
+	return gulp.src([
+		'source/orr/**/*',
+	], { base: 'source' })
+	.pipe(gulp.dest('public/'))
 });
 
 gulp.task('copy-deps', function () {
@@ -25,11 +38,37 @@ gulp.task('copy-deps', function () {
 	.pipe(gulp.dest('public/deps'));
 });
 
-gulp.task('gh-pages', function () {
-	return gulp.src('public/**/*')
-	.pipe(gulp.dest('../one-ring-roller-gh-pages'));
+// generate sprite.png and _sprite.scss
+gulp.task('sprites-dice', function () {
+  return gulp.src('source/images/dice/*.png')
+	.pipe(sprite({
+		name: 'sprite-dice',
+		style: 'sprite-dice.css',
+		cssPath: '',
+		processor: 'css',
+		prefix: 'dice'
+	}))
+	.pipe(gulpif('*.png', gulp.dest('public/orr'), gulp.dest('public/orr')))
 });
 
-gulp.task('default', ['sprites-dice', 'copy-deps'], function() {
-	console.log('Default task running.');
+// Used for "publishing" to live. Requires manual steps in CLI afterwards.
+gulp.task('gh-pages', function () {
+	return gulp.src('public/**/*')
+	.pipe(gulp.dest('../one-ring-roller-gh-pages/'));
 });
+
+gulp.task('build', function(callback) {
+	runSequence(
+		'clean',
+		[
+			'copy-source-assets',
+			'copy-source-code',
+			'sprites-dice',
+			'copy-deps'
+		],
+		callback
+	);
+});
+
+// TODO How about a watch task?
+gulp.task('default', ['build']);
